9d533435aa23268c3e8ed75430e8c1b2de33a1b8
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:57:06 +0300

    Added some comments

--- module/model/Human/IHumanPriority.cs
+++ module/model/Human/IHumanPriority.cs
@@ -4,9 +4,14 @@ using System.Text;
 
 namespace VisitCounter
 {
+    /// <summary>HumanPriority interface.</summary>
     public interface IHumanPriority: IMovingObject
     {
-        public Dictionary<String, IDestinationType> GetDestinatinTypeDic
+        /// <value>
+        /// Property <c>GetDestinatinTypeDic</c> 
+        /// Dictionary of destination types.
+        /// </value>
+        public Dictionary<String, IDestinationType> DestinatinTypeDic
         {
             get;
         }
