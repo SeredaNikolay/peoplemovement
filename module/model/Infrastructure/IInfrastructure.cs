9a9933e8d66aafc6fa4afc045987851f393845da
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:58:20 +0300

    Added some comments

--- module/model/Infrastructure/IInfrastructure.cs
+++ module/model/Infrastructure/IInfrastructure.cs
@@ -2,9 +2,21 @@
 
 namespace VisitCounter
 {
+    /// <summary>Infrastructure interface.</summary>
     public interface IInfrastructure : IHasTagObject, IID, IBaseObject
     {
+        /// <value>
+        /// Property <c>CustomType</c> 
+        /// IInfrastructure CustomizerType.
+        /// </value>
         public CustomizerType CustomType { get; set; }
+
+        /// <summary>Sets buffer for IInfrastructure geometry.</summary>
+        /// <param name="additionalRadius">Buffer radius</param>
+        /// <param name="needsPolygon">
+        /// Polygon flag (true - buffer for polygon geometry;
+        /// false - buffer for linear ring geometry).
+        /// </param>
         public void SetBufferedGeometry(double additionalRadius,
                                         bool needsPolygon);
     }
