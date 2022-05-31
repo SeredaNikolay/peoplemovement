43f4abd4326a4f0131acfb5329801a6e00bd24aa
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:53:33 +0300

    Added some comments

--- module/model/BaseObject/IBaseObject.cs
+++ module/model/BaseObject/IBaseObject.cs
@@ -2,8 +2,12 @@
 
 namespace VisitCounter
 {
+    /// <summary>BaseObject interface.</summary>
     public interface IBaseObject
     {
+        /// <value>
+        /// Property <c>Geometry</c> represents the geometry.
+        /// </value>
         public Geometry Geometry { get; set; }
     }
 }
