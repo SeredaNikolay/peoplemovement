6c77f863725662859f23e6cf846f907df857dade
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:53:17 +0300

    Added some comments

--- module/model/BaseObject/IBase.cs
+++ module/model/BaseObject/IBase.cs
@@ -4,13 +4,34 @@ using System.Text;
 
 namespace VisitCounter
 {
-    /// <summary>
 
+    /// <summary>
+    /// Parent interface for ICommandLine and IDataConverter.
     /// </summary>
     public interface IBase
     {
+        /// <summary>
+        /// Gets main rectangle's infrastructure in dictionary.
+        /// </summary>
+        /// <returns>
+        /// Dictionary of infrastructure with
+        /// string key ("polygon", "way", "point", "pointbuffer") and 
+        /// IInfrastructure list value.
+        /// </returns>
         public Dictionary<String, List<IInfrastructure>> GetInfrastDic();
+
+        /// <summary>
+        /// Gets main rectangle's people in dictionary.
+        /// </summary>
+        /// <returns>
+        /// Dictionary of people with int human ID key and IHuman value.
+        /// </returns>
         public Dictionary<int, IHuman> GetHumanDic();
+
+        /// <summary>
+        /// Gets main rectangle's grid.
+        /// </summary>
+        /// <returns>Gets main rectangle's IGrid.</returns>
         public IGrid GetGrid();
     }
 }
