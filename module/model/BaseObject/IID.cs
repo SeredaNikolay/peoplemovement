f2f1de7254ca2ff7cb8ef620d21438751e885447
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:53:49 +0300

    Added some comments

--- module/model/BaseObject/IID.cs
+++ module/model/BaseObject/IID.cs
@@ -2,8 +2,12 @@
 
 namespace VisitCounter
 {
+    /// <summary>ID interface.</summary>
     public interface IID
     {
+        /// <value>
+        /// Property <c>ID</c> represents the int id.
+        /// </value>
         public int ID { get; }
     }
 }
