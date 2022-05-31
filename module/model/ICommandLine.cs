da0c3e11ba858aeba29567944b65e8ccdcd80b66
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 09:56:21 +0300

    CommandLine interface added

---
+++ module/model/ICommandLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisitCounter
+{
+    interface ICommandLine: IBase
+    {
+        public void AddOsmInfrastructure(bool add);
+        public void AddInfrastructure(String geometryType);
+        public void AddHuman();
+        public void ShowInfrastInfo(String strRadius,
+                                    String strX,
+                                    String strY);
+        public void ShowHumanInfo(String strRadius,
+                                  String strX,
+                                  String strY);
+        public void CreateCSV();
+    }
+}
