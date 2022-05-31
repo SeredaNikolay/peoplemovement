38e3da2269668f701371669a35fafe10ebb1f97d
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:56:28 +0300

    Added some comments

--- module/model/Human/IDestinationType.cs
+++ module/model/Human/IDestinationType.cs
@@ -2,10 +2,25 @@
 
 namespace VisitCounter
 {
+    /// <summary>DestinationType interface.</summary>
     public interface IDestinationType
     {
+        /// <value>
+        /// Property <c>Value</c> 
+        /// Value of attraction to this destination type.
+        /// </value>
         public double Value { get; set; }
+
+        /// <value>
+        /// Property <c>Name</c> 
+        /// Name of this destination type.
+        /// </value>
         public String Name { get; }
+
+        /// <value>
+        /// Property <c>TypePriority</c> 
+        /// Priority of this destination type.
+        /// </value>
         public int TypePriority { get; }
     }
 }
