8ed79a97cdecd5c433106aec624fe39f4d911206
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:57:58 +0300

    Added some comments

--- module/model/Infrastructure/IHasTagObject.cs
+++ module/model/Infrastructure/IHasTagObject.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace VisitCounter
 {
+    /// <summary>
+    /// Interface for classes with Dictionary<String, bool> field.
+    /// </summary>
     public interface IHasTagObject
     {
+        /// <value>
+        /// Property <c>HasTagDic</c> 
+        /// Tag dictionary with key string tag and bool value.
+        /// </value>
         public Dictionary<String, bool> HasTagDic { get; set; }
     }
 }
