287e93f1a76790bb0af155edf3144ae9bed11fa0
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:57:21 +0300

    Added some comments

--- module/model/Human/IMovingObject.cs
+++ module/model/Human/IMovingObject.cs
@@ -3,11 +3,31 @@ using NetTopologySuite.Geometries;
 
 namespace VisitCounter
 {
+    /// <summary>MovingObject interface.</summary>
     public interface IMovingObject
     {
+        /// <value>
+        /// Property <c>DstCoordinates</c> 
+        /// List of destination coordinates.
+        /// </value>
         public List<Coordinate> DstCoordinates { get; set; }
+
+        /// <value>
+        /// Property <c>VisitedCrdList</c> 
+        /// List of visited coordinates.
+        /// </value>
         public List<Coordinate> VisitedCrdList { get; }
+
+        /// <value>
+        /// Property <c>PrevCellI</c> 
+        /// X-axis index of previous visited cell.
+        /// </value>
         public int PrevCellI { get; set; }
+
+        /// <value>
+        /// Property <c>PrevCellJ</c> 
+        /// Y-axis index of previous cell.
+        /// </value>
         public int PrevCellJ { get; set; }
     }
 }
