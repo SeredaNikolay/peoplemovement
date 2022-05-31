eb5063d613549c9a439935ca8bc655ac19a77c89
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:55:50 +0300

    Added some comments

--- module/model/DataConverter/IOsmToInfrastructureConverter.cs
+++ module/model/DataConverter/IOsmToInfrastructureConverter.cs
@@ -4,15 +4,33 @@ using CityDataExpansionModule.OsmGeometries;
 
 namespace VisitCounter
 {
+    /// <summary>OsmToInfrastructureConverter interface.</summary>
     public interface IOsmToInfrastructureConverter
     {
+        /// <summary>Adds walkable tag to infrastructure.</summary>
+        /// <param name="TagsToOutTagsDic">
+        /// Dictionary with string key tag and TagsOfOutTag value.
+        /// </param>
+        /// <param name="infrastObj">IInfrastructure to add tag.</param>
+        /// <param name="osmObj">
+        /// IOsmObject with tags to check for obstacles.
+        /// </param>
         public void AddWalkableTag(
             Dictionary<String, TagsOfOutTag> TagsToOutTagsDic,
             IInfrastructure infrastObj,
-            IOsmObject osmClosedWay);
+            IOsmObject osmObj);
+
+        /// <summary>Adds out tags to infrastructure.</summary>
+        /// <param name="TagsToOutTagsDic">
+        /// Dictionary with string tag and TagsOfOutTag value.
+        /// </param>
+        /// <param name="infrastObj">IInfrastructure to add tag.</param>
+        /// <param name="osmObj">
+        /// IOsmObject with tags to check for out tags.
+        /// </param>
         public void AddOutTags(
             Dictionary<String, TagsOfOutTag> TagsToOutTagsDic,
             IInfrastructure infrastObj,
-            IOsmObject osmClosedWay);
+            IOsmObject osmObj);
     }
 }
