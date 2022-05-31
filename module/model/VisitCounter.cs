1a096dcff6b852b744fe7c001e417318f417f0a6
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 09:45:21 +0300

    Decomposition and file movement

--- module/model/VisitCounter.cs
+++ module/model/VisitCounter.cs
@@ -2,39 +2,62 @@
 using OSMLSGlobalLibrary.Modules;
 using System.Collections.Generic;
 using CityDataExpansionModule;
-using CityDataExpansionModule.OsmGeometries;
 using NetTopologySuite.Geometries;
-using System.IO;
-using System.Text;
 
 namespace VisitCounter
 {
     public class VisitCounter : OSMLSModule
     {
-        City _city = null;
         long _oldMs = 0;
         bool _readLineIsActive = false;
         int _iterationCount = 0;
         bool _updated = true;
         String _command;
-        String _path = Directory.GetCurrentDirectory();
-        String _dirSep = Path.DirectorySeparatorChar.ToString();
-        String _filename = "visits.csv";
+        IGrid _grid;
+
+        ICommandLine _commandLine;
         protected override void Initialize()
         {
-            this._city = new City(MapObjects, 
-                GetModule<CityDataExpansion>().MapBorders);
+            Polygon mainRectangle = GetModule<CityDataExpansion>()
+                .MapBorders;
+            this._commandLine = new CommandLine(MapObjects,
+                                                        mainRectangle);
+            this._grid = this._commandLine.GetGrid();
+            IHuman human;
+            if (this._commandLine.GetHumanDic().TryGetValue(1, out human))
+            {
+                human.SetDestinationCoordinates(
+                    new List<Coordinate>()
+                    {
+                        new Coordinate(4194240, 7516324)
+                    });
+            }
+            if (this._commandLine.GetHumanDic().TryGetValue(2, out human))
+            {
+                human.SetDestinationCoordinates(
+                    new List<Coordinate>()
+                    {
+                        new Coordinate(4194302, 7516220),
+                        new Coordinate(4194296, 7516230)
+                    });
+            }
         }
-        
         public override void Update(long elapsedMilliseconds)
         {
             if (this._updated && elapsedMilliseconds - this._oldMs >= 500)
             {
+                int j, i;
                 this._updated = false;
-                foreach (KeyValuePair<int, Human> pair in Human.HumanDic)
+                foreach (KeyValuePair<int, IHuman> pair 
+                    in this._commandLine.GetHumanDic())
                 {
                     pair.Value.Live(null);
-                    //this._mapObjects.Add(new Point(((Point)pair.Value.Geometry).Coordinate.X, ((Point)pair.Value.Geometry).Coordinate.Y));
+                    j = pair.Value.PrevCellJ;
+                    i = pair.Value.PrevCellI;
+                    this._grid.AddVisitsToCells(pair.Value.VisitedCrdList,
+                                                ref j, ref i);
+                    pair.Value.PrevCellJ = j;
+                    pair.Value.PrevCellI = i;
                 }
                 this._iterationCount++;
                 this._updated = true;
@@ -45,25 +68,7 @@ namespace VisitCounter
                     this._command = Console.ReadLine();
                     if (this._command == "csv")
                     {
-                        using (FileStream fs = File.Create(this._path + this._dirSep + this._filename))
-                        {
-                            Grid grid = Grid.GridInstance;
-                            for (int j = 0; j < grid.GetCellCountY; j++)
-                            {
-                                String csvSep = ",";
-                                for (int i = 0; i < grid.GetCellCountX; i++)
-                                {
-                                    if (i == grid.GetCellCountX - 1)
-                                    {
-                                        csvSep = "\n";
-                                    }
-                                    int count = grid.GetCellByIndexes(j, i).VisitCount;
-                                    byte[] bytes = new UTF8Encoding().GetBytes(count.ToString() + csvSep);
-                                    fs.Write(bytes, 0, bytes.Length);
-                                }
-                            }
-                        }
-                        Console.WriteLine("CSV created");
+                        this._commandLine.CreateCSV();
                     }
                     this._readLineIsActive = false;
                 }
