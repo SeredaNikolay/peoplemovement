0922a3dcd64ddae4ba248fc278ee33adea7f1e8d
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:56:45 +0300

    Added some comments

--- module/model/Human/IHuman.cs
+++ module/model/Human/IHuman.cs
@@ -5,9 +5,19 @@ using NetTopologySuite.Geometries;
 
 namespace VisitCounter
 {
+    /// <summary>Human interface.</summary>
     public interface IHuman : IID, IBaseObject, IHumanPriority
     {
+        /// <summary>Sets list of destination coordinates.</summary>
+        /// <param name="dstCrdList">
+        /// List of destination coordinates.
+        /// </param>
         public void SetDestinationCoordinates(List<Coordinate> dstCrdList);
+
+        /// <summary>Performs an iteration for human.</summary>
+        /// <param name="delegateArg">
+        /// List of double arguments to perform an iteration.
+        /// </param>
         public void Live(List<double> delegateArg);
     }
 }
