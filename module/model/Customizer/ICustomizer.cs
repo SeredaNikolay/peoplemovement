7851ef9809efc4cdf1a48d8cba1e1808c9c7dd3d
SeredaNikolay <56348420+SeredaNikolay@users.noreply.github.com>
2022-05-31 20:54:02 +0300

    Added some comments

--- module/model/Customizer/ICustomizer.cs
+++ module/model/Customizer/ICustomizer.cs
@@ -4,10 +4,25 @@ using NetTopologySuite.Geometries;
 
 namespace VisitCounter
 {
+    /// <summary>Customizer interface.</summary>
     public interface ICustomizer
     {
+        /// <summary>
+        /// Gets CustomizerType to create customaized geometry.
+        /// </summary>
+        /// <param name="hasTagDic">
+        /// Tag dictionary with key string tag and bool value.
+        /// </param>
+        /// <returns>
+        /// CustomizerType to create customaized geometry.
+        /// </returns>
         public CustomizerType GetCustomizerTypeByTags(
             Dictionary<String, bool> hasTagDic);
+
+        /// <summary>Gets customized geometry.</summary>
+        /// <param name="geometry">Geometry for customization.</param>
+        /// <param name="objType">CustomizerType for customization.</param>
+        /// <returns> Customized geometry.</returns>
         public Geometry Customize(Geometry geometry,
                           CustomizerType objType);
     }
